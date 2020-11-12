b2ab8f92cf20f26b00c5eabe3cd4a3b7d5ff4bd2
ZiiBot17 <74260816+ZiiBot17@users.noreply.github.com>
2020-11-12 22:40:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'Bott Zii☄'; 
+const BotName = '🔰Bott Zii🔰'; 
 const instagram = 'https://instagram.com/M.Alfrzyy_'; 
 const telegram = 'https://t.me/Zii'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
