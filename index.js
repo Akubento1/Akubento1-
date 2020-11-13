067f4e447dbcc4011f83191ace9c7594d0af6710
ZiiBot17 <74260816+ZiiBot17@users.noreply.github.com>
2020-11-13 11:19:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = '🔰Bott Zii🔰'; 
+const BotName = 'Bott Zii🖕'; 
 const instagram = 'https://instagram.com/M.Alfrzyy_'; 
 const telegram = 'https://t.me/Zii'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
@@ -68,7 +68,7 @@ conn.on('user-presence-update', json => console.log(json.id + ' presence is => '
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
-   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@reuz.id`)
+   console.log(`[ ${moment().format("HH:mm:ss")} ] => bot by ig:@m.alfrzyy_`)
 })
 
 conn.on('message-new', async(m) =>
